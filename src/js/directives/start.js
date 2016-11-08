6564029dc1a87e3939ab5ad661857f2de23d7f5b
caowencheng <845982120@qq.com>
2016-11-08 08:37:38 +0800

    指令

--- src/js/directives/start.js
+++ src/js/directives/start.js
@@ -5,7 +5,7 @@ myApp.directive('start',[function(){
 	return {
 	    restrict:'AECM',
 	    scope:true,
-	    template:"<div><input type='checkbox' ng-checked = '{{checkBox}}'/>cwc</div>",
+	    template:"<div><input type='checkbox'/>cwc</div>",
 	    link:function(scope,element){
 	    	element.bind('click',function(){
 	    		console.log("我点击了指令！");
